6ebc5e21bac5e8bc847090207014bfb09c9d093d
rybaz <>
2025-02-06 12:02:31 -0700

    only works on linux apparently

--- 4-impacket-devshell/flake.nix
+++ 4-impacket-devshell/flake.nix
@@ -1,33 +1,16 @@
 {
-  description = "A very basic flake";
+  description = "Build Shell with any dependency of the project";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs }: 
   let
-    pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
   in
   {
-    devShells."aarch64-darwin".default = pkgs.mkShell {
-      packages = with pkgs; [ 
-        # python
-        pkgs.python312
-        # python modules
-        pkgs.python312Packages.impacket
-        pkgs.python312Packages.setuptools
-        pkgs.python312Packages.six
-        pkgs.python312Packages.charset-normalizer
-        pkgs.python312Packages.pyasn1
-        pkgs.python312Packages.pyasn1-modules
-        pkgs.python312Packages.pycryptodomex
-        pkgs.python312Packages.pyopenssl
-        pkgs.python312Packages.ldap3
-        pkgs.python312Packages.ldapdomaindump
-        pkgs.python312Packages.flask
-        # pkgs.python312Packages.pyreadline3
-	    ];
-    };
+    devShells."x86_64-linux".default = 
+      import ./shell.nix {inherit pkgs; };
   };
 }
