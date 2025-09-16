e9ee2e5cbebc7eeed6ec29aee2caaec8c462ce81
rybaz <>
2025-09-16 12:44:34 -0400

    adding labs

---
+++ 0.4-portable-hackshell/hack.nix
@@ -0,0 +1,45 @@
+ { pkgs ? import <nixpkgs> {} }:
+
+  pkgs.mkShell {
+
+    nativeBuildInputs = with pkgs.buildPackages; [
+      # core
+      bat
+      coreutils
+      curl
+      fd
+      fzf
+      git
+      jq
+      ripgrep
+      tree
+      wget
+
+      # general
+      kubectl
+      mitmproxy
+      nmap
+
+      # disvovery
+      dnsx
+      httpx
+      ipinfo
+      naabu
+      subfinder
+
+      # vulns
+      nuclei
+      metasploit
+      sqlmap
+
+      # screenshot flyovers
+      gowitness
+      #google-chrome
+
+      # active directory
+      mitm6
+      coercer
+      responder
+      bloodhound-py
+    ];
+}
--- 04-remote-config/README.md
+++
@@ -1,2 +0,0 @@
-# WIP
-This module will not fully work yet, but I will continue to update this as I build out the talk more.
